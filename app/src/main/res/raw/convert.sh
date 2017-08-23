2e5cce08b007c7bcef9da6e914bb777284eb1fdd
Mikhail Kulesh <mikhail.kulesh@gmail.com>
2017-08-23 17:25:04 +0200

    #2: Update icons

--- app/src/main/res/raw/convert.sh
+++ app/src/main/res/raw/convert.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 
-find . -name "*.svg" ! -name "formula*" -exec ./convert_file.sh {} mdpi 32 \;
-find . -name "*.svg" ! -name "formula*" -exec ./convert_file.sh {} hdpi 48 \;
-find . -name "*.svg" ! -name "formula*" -exec ./convert_file.sh {} xhdpi 64 \;
-find . -name "*.svg" ! -name "formula*" -exec ./convert_file.sh {} xxhdpi 96 \;
+find . -name "*.svg" ! -name "formula*" -exec ./convert_single.sh {} \;
 
 ./convert_file.sh ./ic_launcher.svg mdpi 48
 ./convert_file.sh ./ic_launcher.svg hdpi 72
--- app/src/main/res/raw/convert_file.sh
+++ app/src/main/res/raw/convert_file.sh
@@ -2,6 +2,6 @@
 
 NAME1=${1%.svg}
 TARGET="../drawable-"${2}"/"${NAME1:2}".png"
-echo Converting to ${TARGET}
+echo Converting to ${1} to png and plains SVG: ${TARGET}
+inkscape --vacuum-defs --export-plain-svg=${1} ${1}
 inkscape --export-png ${TARGET} -w ${3} -h ${3} ${1}
-
